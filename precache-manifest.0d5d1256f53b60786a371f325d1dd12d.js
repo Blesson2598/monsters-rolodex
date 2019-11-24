55f747727243a02b71a3310ca9362dd4fc5a1c9a
Chris Blesson <blesson2598@gmail.com>
2019-11-24 16:57:16 +0530

    Updates

---
+++ precache-manifest.0d5d1256f53b60786a371f325d1dd12d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bd5d7f9029240e192f3721dc34d68d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4f6490f3e2703da4a67d",
+    "url": "/monsters-rolodex/static/css/main.7c32374c.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "4f6490f3e2703da4a67d",
+    "url": "/monsters-rolodex/static/js/main.a4c6631c.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
